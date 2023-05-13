547322e768a7dd34a49dc56f69ab7fad6c564a98
baaioy <balotech514@gmail.com>
2023-05-13 18:43:04 +0100

    Practice the write function of streamlit - challenge day 5

---
+++ day_5/streamlit_app.py
@@ -0,0 +1,40 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.write('Header')
+
+# Example 1
+
+st.write("Hello *world!* :sunglasses:")
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+
+st.write(df)
+
+# Example 4
+
+st.write("Below is a dataframe", df, "Above is a dataframe")
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
+
+# Selt learning
+
+st.latex('Hello **world!** :smile:')
